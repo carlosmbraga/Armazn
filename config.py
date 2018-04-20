4528516b6d3013adeae458f76289e0f14321a966
Gabriel Pacheco <gabrielpacheco23@live.com>
2018-04-20 15:15:46 -0300

    [MODELS] Consertando import do db

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = 'mysql://root:1234@localhost/testsupport'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:3306@localhost/armazn'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 SECRET_KEY = os.urandom(24)
