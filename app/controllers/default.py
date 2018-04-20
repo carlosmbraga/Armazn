118743b13cff4a6f22c756941fe3e19f2d881f08
Carlos Braga <eddbraga@hotmail.com>
2018-04-20 04:37:08 -0300

    [LAYOUT] Add novos templates para futura implementação

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -5,5 +5,18 @@ from flask import Flask, request, redirect, url_for, session, g, flash, \
 
 @app.route('/')
 def index():
-    return render_template('layout.html')
+    return render_template('index.html')
 
+@app.route('/registro')
+def registro():
+    return render_template('registro.html')
+	
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+	
+
+
+
+	
