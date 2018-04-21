2585e3a7723630fbecc6a899601632bcdb65723f
Gabriel Pacheco <gabrielpacheco23@live.com>
2018-04-20 23:23:25 -0300

    [MODELS] Inserida a tabela 'Files'

--- app/models/tables.py
+++ app/models/tables.py
@@ -15,3 +15,22 @@ class User(db.Model):
 
 	def __repr__(self):
 		return "<User %r>" % self.username
+
+
+class File(db.Model):
+	__tablename__ = "files"
+
+	id = db.Column(db.Integer, primary_key=True)
+	filename = db.Column(db.String, unique=True)
+	size = db.Column(db.Numeric, unique=True)
+	owner_id = db.Column(db.Intenger, db.ForeignKey('users.id'))
+
+	owner = db.relationship('User', foreign_keys=owner_id)
+
+	def __init__(self, filename, size, owner_id):
+		self.filename = filename
+		self.size = size
+		self.owner_id = owner_id
+
+	def __repr__(self):
+		return "<File %r>" % self.id
