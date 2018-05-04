b1fd5a4ae1dec2b8698af345df73af5b860eac9c
Carlos Braga <eddbraga@hotmail.com>
2018-05-04 01:23:51 -0300

    Ajuste Controller default e base layout

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,34 +1,19 @@
-from flask import render_template
+import os
 from app import app, db, lm
-from flask import Flask, request, redirect, url_for, session, g, flash, \
-<<<<<<< HEAD
-     render_template
+from flask import Flask, request, redirect, url_for, session, render_template, send_from_directory, flash
 from flask_login import login_user, logout_user	 
-||||||| merged common ancestors
-     render_template
-=======
-     render_template, send_from_directory
 from werkzeug.utils import secure_filename
->>>>>>> e20e1dead3d92c65b2e09f4343c7eaaf8d20b145
 from app.models import forms
 from app.models import tables
-<<<<<<< HEAD
 from app.models.tables import User
-
-
-@lm.user_loader
-def load_user(id):
-	return User.query.filter_by(id=id).first()
-||||||| merged common ancestors
-=======
-import os
-
 
 UPLOAD_FOLDER = 'app/uploads'
 ALLOWED_EXTENSIONS = set(['txt', 'png'])
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
->>>>>>> e20e1dead3d92c65b2e09f4343c7eaaf8d20b145
+@lm.user_loader
+def load_user(id):
+	return User.query.filter_by(id=id).first()
 
 @app.route('/')
 def index():
@@ -37,7 +22,6 @@ def index():
 @app.route('/registro', methods=('GET', 'POST'))
 def registro():
     form = forms.RegisterForm()
-<<<<<<< HEAD
     if request.method == "POST":
         username=form.username.data,
         email=form.email.data,
@@ -52,30 +36,7 @@ def registro():
             flash("Falha no registro", "error")
     else:
         return render_template('registro.html', form=form)
-	
-||||||| merged common ancestors
-    if form.validate_on_submit():
-        flash("Registro realizado com sucesso!", "success")
-        forms.create_user(
-            username=form.username.data,
-            email=form.email.data,
-            password=form.password.data
-        )
-        return redirect(url_for('login'))
-    return render_template('registro.html', form=form)
-	
-=======
-    if form.validate_on_submit():
-        flash("Registro realizado com sucesso!", "success")
-        forms.create_user(
-            username=form.username.data,
-            email=form.email.data,
-            password=form.password.data
-        )
-        return redirect(url_for('login'))
-    return render_template('registro.html', form=form)
 
->>>>>>> e20e1dead3d92c65b2e09f4343c7eaaf8d20b145
 @app.route('/login', methods=('GET', 'POST'))
 def login():
     form = forms.LoginForm()
@@ -90,21 +51,11 @@ def login():
     else:
         return render_template('login.html', form=form)
 
-<<<<<<< HEAD
 @app.route('/logout')
 def logout():
 	logout_user()
 	return redirect(url_for('index'))
-	
-	
-@app.errorhandler(404)
-def page_not_found(e):
-  return render_template('error.html'), 404	
-||||||| merged common ancestors
-@app.errorhandler(404)
-def page_not_found(e):
-  return render_template('error.html'), 404	
-=======
+
 # abaixo, /uploads e /uploads/<filename>: tratam do upload e armazenamento dos arquivos
 def allowed_file(filename):
     return '.' in filename and \
@@ -126,7 +77,6 @@ def upload_file():
             return redirect(url_for('uploaded_file', filename=filename))
 
     return render_template('upload.html')
->>>>>>> e20e1dead3d92c65b2e09f4343c7eaaf8d20b145
 
 # precisa de melhoria do layout dessas paginas de upload, mas o funcionamento está ok
 # (eles salvam na pasta uploads que eu criei no diretório do app) e
@@ -136,8 +86,6 @@ def upload_file():
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
-
-# error handling
 @app.errorhandler(404)
 def page_not_found(e):
-  return render_template('error.html'), 404
+  return render_template('error.html'), 404	
