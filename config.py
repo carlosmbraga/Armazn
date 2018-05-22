5c24e1b5a13045832f64bb59c05b20a4d712b01d
Carlos Braga <eddbraga@hotmail.com>
2018-05-22 20:36:10 -0300

    ajuste senha do BD

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = 'mysql://root:123qweasdzxc@localhost:3306/armazn'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:1234@localhost:3306/armazn'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 SECRET_KEY = os.urandom(24)
